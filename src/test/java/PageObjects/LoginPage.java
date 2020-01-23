a6e3b9bf70afb7a7c2ab731e233311ba79b6d548
nur-sayed <nur.sayed@outlook.com>
2020-01-22 22:50:34 -0500

    test commit

---
+++ src/test/java/PageObjects/LoginPage.java
@@ -0,0 +1,17 @@
+package PageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+	WebDriver ldriver;
+	LoginPage(WebDriver rdriver)
+	{
+	
+	ldriver=rdriver;
+	PageFactory.initElements(rdriver, this);
+	
+	
+}
+}
---
+++ src/test/java/TestCases/TC001_VerifyLoginFunctionality.java
@@ -0,0 +1,18 @@
+package TestCases;
+
+public class TC001_VerifyLoginFunctionality {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 20;
+		int y = 18;
+		if (x > y) {
+		  System.out.println("x is greater than y");
+		}
+		
+		
+		
+	}
+
+}
